49d8c9dce5c8da088a9b006e1a0578f8341244d5
Helene Engler <HeleneEngler@Helenes-MacBook-Pro-4.local>
2021-11-21 22:58:38 +0000

    added changes

---
+++ Untitled.R
@@ -0,0 +1,17 @@
+
+#Loading required R packages
+
+library(tidyverse) #data manipulation and visualization
+library(caret) #machine learning workflow
+library(leaps) #computing stepwise regression
+
+
+
+
+
+
+
+
+
+
+
