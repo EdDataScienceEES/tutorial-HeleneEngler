92881ede5326fb6e73ba8c292d5e7c6eb856dbea
Helene Engler <s1616894@ed.ac.uk>
2021-12-08 19:55:14 +0100

    finalize script

--- Scripts/plant_traits.R
+++ Scripts/plant_traits.R
@@ -1,5 +1,9 @@
-#Stepwise regression Analysis Tutorial 
+# Multiple Regression Analysis Tutorial 
 ## by Helene Engler 
+
+# Learning outcomes:
+## Learn how to perform Hierarchical Regression Analysis 
+## Learn how to perform Stepwise Regression Analysis 
 
 #Loading required R packages ----
 library(tidyverse)  # Data manipulation and visualization
@@ -7,6 +11,7 @@ library(ggplot2)    # Data visualisation
 library(lme4)       # Linear Models 
 library(janitor)    # Data cleaning
 library(olsrr)      # Stepwise regression analsysis
+library(MASS)       # Stepwise regression analsysis
 
 ##Install necessary packages
 #install.packages("tidyverse")
@@ -14,6 +19,7 @@ library(olsrr)      # Stepwise regression analsysis
 #install.packages("lme4")
 #install.packages("janitor")
 #install.packages("olsrr")
+#install.packages("MASS")
 
 # Set your Working Directory 
 setwd("/Users/HeleneEngler/University/R/tutorial-HeleneEngler")
@@ -31,74 +37,75 @@ ncol(traits)
 ### Plot Histogram in basic R 
 hist(traits$height, breaks = 10) # non normal distribution, right skew 
 
-### Log transforming data, to achieve normal distribution
+# Log transforming data, to achieve normal distribution
 traits <-  traits %>%
   mutate(log.ht = log(height))
 
-#### Check log distribtuion 
+# Check log distribtuion 
 hist(traits$log.ht, breaks = 10) # close to normal 
 
-#### Plot Histogram with ggplot2
+# Plot Histogram with ggplot2
 ggplot(traits, aes(x=log.ht)) + 
   geom_histogram(bins = 30)     
 
-### Shapiro test 
+# Shapiro test 
 shapiro.test(traits$log.ht)       # p < 0.05 indicates non-normal distribuion
-# but usually linear models are not too sensitive, we will push on and look at the residuals
-# to determine if assumptions have been met 
+## but usually linear models are not too sensitive, we will push on and look at the residuals
+## to determine if assumptions have been met 
 
 # Linear model creation ----
-## Null model 
+# Null model 
 model.null <- lm(log.ht ~ 1, data=traits)
 
-## Simple univariate model
+# Simple univariate model
 model.1 <- lm(log.ht ~ temp, data=traits)
 
-### Check predcitive power 
+# Check predcitive power 
 AIC(model.null, model.1)
 
-### Check if assumptions are met 
+# Check if assumptions are met 
 resid1 <-  resid(model.1)
 plot(resid1)                # Equal variance, no observable patterns
 plot(model.1)               # Model assumptions are met, some outliers, 
                             # but none outside Cook´s distance (residuls vs leverage)
 shapiro.test(resid1)        # p > 0.05, normally distributed residuals
 
-#### Add on to the model
-model.2 <- lm(log.ht ~ temp + rain, data=traits) # Include rain
-AIC(model.null, model.1, model.2)                # Addition of rain improves model
+# Add on to the model
+model.2 <- lm(log.ht ~ temp + rain, data=traits)                # Include rain
+model.3 <- lm(log.ht ~ temp + rain + alt, data=traits)          # Include altitude
+model.4 <- lm(log.ht ~ temp + rain + LAI, data=traits)          # Include LAI
+model.5 <- lm(log.ht ~ temp + rain + NPP, data=traits)          # Include NPP
+model.6 <- lm(log.ht ~ temp + rain + hemisphere, data=traits)   # Include hemisphere
+model.7 <- lm(log.ht ~ temp + rain + isotherm, data=traits)     # Include isotherm
+model.8 <- lm(log.ht ~ temp + rain + hemisphere + LAI + alt + NPP + isotherm, data=traits) # Include all
+##...
 
-model.3 <- lm(log.ht ~ temp + rain + alt, data=traits) # Include altitude
-AIC(model.null, model.1, model.3, model.4)             # Altitude does not improve model fit, discard
+AIC(model.null, model.1, model.3, model.4, model.5, model.6, model.7, model.8)                
 
-model.4 <- lm(log.ht ~ temp + rain + hemisphere, data=traits) # Include hemisphere
-AIC(model.null, model.1, model.3, model.4)                    # Hemisphere improves model 
+# Check residuals 
+resid4 <-  resid(model.4)
+plot(resid1)                # Equal variance, no observable patterns
+plot(model.4)               # Model assumptions are met, some outliers (e.g.6,96,146)
+                            # but none outside Cook´s distance (residuls vs leverage)
+shapiro.test(resid4)        # p>0.05 = normal distribution 
+
 
 # Stepwise regression analysis ----
-## Define null model
-null.model <- lm(log.ht ~ 1, data=traits)
 
-## Define model with all predictors that may influence the dependent variable
-step.model <- lm(log.ht ~ alt + temp + rain + LAI + NPP + hemisphere, data=traits)
-#all <- lm(log.ht ~ ., data=traits) --> this way all variables will be checked
-# here there are a lot, many more than 2 cathegories (not possible), 
-# it takes a long time to compute too many variables
+# Define model with all predictors that may influence the dependent variable
+step.model <- lm(log.ht ~ alt + temp + rain + LAI + NPP + hemisphere + isotherm, data=traits)
+## all <- lm(log.ht ~ ., data=traits) --> this way all variables will be checked
+## The plant trait data set includes a number of parameters with little ecological value 
+## e.g. sort number, those are unceseary to include
+## Having a lot of parameters also takes a long time to compute 
 
-### SRA with olsrr package
-SRA <-ols_step_all_possible(step.model)
+# SRA with MASS package 
+step_traits <- stepAIC(step.model, trace = TRUE, direction= "both")    # both directions
+# step_traits_b <- stepAIC(step.model, trace = TRUE, direction= "backward") # backward selection
+# step_traits_f <- stepAIC(step.model, trace = TRUE, direction= "forward") # forward selection
+## only works with the 'all model' 
+
+# SRA with olsrr package
+SRA <- ols_step_best_subset(step.model)
 SRA
 plot(SRA)
-
-#### to compare AIC
-ols_step_best_subset(step.model)
-
-# MASS packacge 
-library(MASS)
-step_traits <- stepAIC(step.model, trace = TRUE, direction= "forward")
-
-### Forward selection process 
-forward <- step(null.model, direction='forward', scope=formula(all), trace=0)
-### Backward selection process 
-backward <- step(null.model, direction='backward', scope=formula(all), trace=0)
-### Forward & Backward selection process 
-both <- step(null.model, direction='both', scope=formula(all), trace=0)
